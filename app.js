1d459b78109766f9374924483a2834626a9f6c62
zephyrus21 <pandey.piyus@gmail.com>
2021-11-23 16:43:36 +0530

    adding /login

--- app.js
+++ app.js
@@ -42,7 +42,37 @@ app.post("/register", async (req, res) => {
 
     user.token = token;
 
+    user.password = undefined;
+
     res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) res.status(400).send("Missing required fields");
+
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (!user) return res.status(400).send("User doesn't exist");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.status(400).send("Incorrect password");
+
+    const token = jwt.sign({ user_id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "4h",
+    });
+
+    user.token = token;
+
+    user.password = undefined;
+
+    res.status(200).json(user);
   } catch (error) {
     console.log(error);
   }
