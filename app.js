8901ca004fec194335559e65d0ba6f157af5daa9
zephyrus21 <pandey.piyus@gmail.com>
2021-11-23 15:14:19 +0530

    adding jswt tokens

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const { connect } = require("./config/database");
 connect();
 
 const express = require("express");
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require("jsonwebtoken");
+
 const User = require("./model/user");
 
 const app = express();
@@ -14,23 +17,35 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-  if (!(email && password && firstName && lastName))
-    res.status(400).send("Missing required fields");
+    if (!(email && password && firstName && lastName))
+      res.status(400).send("Missing required fields");
 
-  const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-  if (existingUser) return res.status(400).send("User already exists");
+    if (existingUser) return res.status(400).send("User already exists");
 
-  const encryptedPassword = await bcrypt.hash(password, 10);
+    const encryptedPassword = await bcrypt.hash(password, 10);
 
-  const user = await User.create({
-    firstName,
-    lastName,
-    email: email.toLowerCase(),
-    password: encryptedPassword,
-  });
+    const user = await User.create({
+      firstName,
+      lastName,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    const token = jwt.sign({ user_id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "4h",
+    });
+
+    user.token = token;
+
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = app;
